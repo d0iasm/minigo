e43f9c43d34e2abdf5238afbb1bb66e3eda49d95
Asami Doi <doiasami1219@gmail.com>
2019-08-24 15:58:08 +0900

    Add tokenizer.

--- main.go
+++ main.go
@@ -3,9 +3,49 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 var in string
+
+type TokenKind int
+
+const (
+	TK_RESERVED = iota
+	TK_NUM
+)
+
+type Token struct {
+	kind TokenKind
+	val  string
+        num int
+}
+
+func tokenize() []Token {
+	tokens := make([]Token, 0)
+	for len(in) > 0 {
+		if in[0] == ' ' {
+			in = in[1:]
+			continue
+		}
+		if in[0] == '+' || in[0] == '-' {
+			tokens = append(tokens, Token{TK_RESERVED, string(in[0]), -1})
+			in = in[1:]
+			continue
+		}
+		if isInt() {
+			tokens = append(tokens, Token{TK_NUM, "", toInt()})
+			continue
+		}
+		panic("Unexcected character: " + string(in[0]))
+	}
+	return tokens
+}
+
+func isInt() bool {
+	_, err := strconv.Atoi(string(in[0]))
+        return err == nil
+}
 
 func toInt() int {
 	n := 0
@@ -22,20 +62,23 @@ func main() {
 	}
 	in = os.Args[1]
 
+	tokens := tokenize()
+
 	fmt.Printf(".intel_syntax noprefix\n")
 	fmt.Printf(".global _main\n")
 	fmt.Printf("_main:\n")
-	fmt.Printf("  mov rax, %d\n", toInt())
-	for len(in) > 0 {
-		switch in[0] {
-		case '+':
-			in = in[1:]
-			fmt.Printf("  add rax, %d\n", toInt())
-		case '-':
-			in = in[1:]
-			fmt.Printf("  sub rax, %d\n", toInt())
+	fmt.Printf("  mov rax, %d\n", tokens[0].num)
+	tokens = tokens[1:]
+	for len(tokens) > 0 {
+		switch tokens[0].val {
+		case "+":
+			fmt.Printf("  add rax, %d\n", tokens[1].num)
+			tokens = tokens[2:]
+		case "-":
+			fmt.Printf("  sub rax, %d\n", tokens[1].num)
+			tokens = tokens[2:]
 		default:
-			panic("Unexcected character: " + string(in[0]))
+			panic("Unexcected character: " + tokens[0].val)
 		}
 	}
 	fmt.Printf("  ret\n")
--- test.sh
+++ test.sh
@@ -19,5 +19,6 @@ assert() {
 assert 0 0
 assert 42 42
 assert 21 '5+20-4'
+assert 2 '1 + 3 - 2'
 
 echo OK
